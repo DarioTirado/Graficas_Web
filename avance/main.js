a6f8309301382876811867ed156e5feefad996cf
Dario Tirado <xdarksx.514@gmail.com>
2023-10-16 01:06:11 -0600

    MODELOS
    Modelos agregados, e iluminacion spoth lght,

--- avance/main.js
+++ avance/main.js
@@ -5,7 +5,7 @@ import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-//camera.position.set(0, 3, 5); // Ajusta la posición de la cámara (x, y, z) según tus necesidades
+camera.position.set(0, -49, 50); // Ajusta la posición de la cámara (x, y, z) según tus necesidades
 var move = true;
 var vida=100;
 const rotationSpeed = -0.50;
@@ -36,6 +36,7 @@ manager.onError = function ( url ) {
 
 };
 
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -45,85 +46,27 @@ const group = new THREE.Group();
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-
-cube.position.set(0, 30, 0); // Reemplaza x, y, z con las coordenadas deseadas
-
+cube.position.set(0, -49, 50); 
 group.add( cube );
 
-var cubeBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-cubeBB.setFromObject(cube);
-
-//esfera
-const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
-const material2 = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-const cube2 = new THREE.Mesh( geometry2, material2 );
-
-group.add( cube2 );
-cube2.position.x=-5;
-
-var cube2BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-cube2BB.setFromObject(cube2);
-group.add( cube2 );
 
 
-//cono 
-const geometry_cono = new THREE.ConeGeometry( 5, 20, 32 ); 
-const material_cono = new THREE.MeshPhongMaterial( {color: 0xffff00} );
-const cone = new THREE.Mesh(geometry_cono, material_cono ); 
-group.add( cone );
-var cube4BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-cube4BB.setFromObject(cone);
-
-cone.scale.copy(new THREE.Vector3(0.05, 0.05, 0.05));
-cone.position.z=-5;
-cone.position.x=0;
-
-
-const geometry4 = new THREE.BoxGeometry( 1, 1, 1 );
-const material4 = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-const cube4 = new THREE.Mesh( geometry4, material4 );
-
-group.add( cube4 );
-cube4.position.x=+8;
-
-var cube4BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-cube4BB.setFromObject(cube4);
-
-
-//cilindro
-const geometry_C = new THREE.CylinderGeometry( 5, 5, 20, 32 ); 
-const material_c = new THREE.MeshPhongMaterial( {color: 0xffff00} ); 
-const cylinder = new THREE.Mesh( geometry_C, material_c );
- group.add( cylinder );
-
-cylinder.scale.copy(new THREE.Vector3(0.05, 0.05, 0.05));
-cylinder.position.x= 2;
-camera.position.z = 5;
-var bandera = false;
-var regreso = false;
-
-var cube3BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-cube3BB.setFromObject(cylinder);
-
-
+//LUCES//
 const al = new THREE.AmbientLight(0xffffff, 0.3);
 group.add(al);
 
-
 const dl = new THREE.DirectionalLight( 0xffffff, 0.6);
-dl.position.set(0,5,0);
+dl.position.set(0,-49,30);
 const dlhelper = new THREE.DirectionalLightHelper(dl,5);
 group.add(dl, dlhelper);
 
-
 const sl = new THREE.SpotLight(0xffffff,3,5,Math.PI/8,0);
-sl.position.set(0,2,0);
+sl.position.set(15,-58,35);
 const SpotLightHelper = new THREE.SpotLightHelper(sl);
 group.add(sl,SpotLightHelper);
 
-
-const pl = new THREE.PointLight(0xffffff, 3, 10 ,0);
-pl.position.set(-5,-3,0);
+const pl = new THREE.PointLight(0xd88d2c, 10, 50 ,0);
+pl.position.set(0,-59,10);
 const pointlightHelper = new THREE.PointLightHelper(pl);
 group.add(pl, pointlightHelper);
 
@@ -133,28 +76,28 @@ var esta_logeado = false;
 camera.rotation.x += rotationSpeed;
 Modelos3D();
 
-
+//HORIZONTE//
 const textureLoader = new THREE.TextureLoader();
 const texture1 = textureLoader.load('media/fondogw.jpg'); // Ruta de la primera textura
 const texture2 = textureLoader.load('media/grass_tex.jpg'); 
 const texture3 = textureLoader.load('media/fondogw.jpg'); 
 const texture4 = textureLoader.load('media/fondogw.jpg'); 
-const texture5 = textureLoader.load('media/fondogw.jpg'); 
+//const texture5 = textureLoader.load('media/fondogw.jpg'); 
 
 
 const greenMaterial1 = new THREE.MeshPhongMaterial({ map: texture1 });
 const greenMaterial2 = new THREE.MeshPhongMaterial({ map: texture2 });
-const greenMaterial3 = new THREE.MeshPhongMaterial({ map: texture3 });
-const greenMaterial4 = new THREE.MeshPhongMaterial({ map: texture4 });
-const greenMaterial5 = new THREE.MeshPhongMaterial({ map: texture5 });
+//const greenMaterial3 = new THREE.MeshPhongMaterial({ map: texture3 });
+//const greenMaterial4 = new THREE.MeshPhongMaterial({ map: texture4 });
+//const greenMaterial5 = new THREE.MeshPhongMaterial({ map: texture5 });
 
 
 const greenPlaneGeometry = new THREE.PlaneGeometry(100, 100); // Ajusta el tamaño del plano 
 const greenPlane1 = new THREE.Mesh(greenPlaneGeometry, greenMaterial1);
 const greenPlane2 = new THREE.Mesh(greenPlaneGeometry, greenMaterial2);
-const greenPlane3 = new THREE.Mesh(greenPlaneGeometry, greenMaterial3);
-const greenPlane4 = new THREE.Mesh(greenPlaneGeometry, greenMaterial4);
-const greenPlane5 = new THREE.Mesh(greenPlaneGeometry, greenMaterial5);
+//const greenPlane3 = new THREE.Mesh(greenPlaneGeometry, greenMaterial3);
+//const greenPlane4 = new THREE.Mesh(greenPlaneGeometry, greenMaterial4);
+//const greenPlane5 = new THREE.Mesh(greenPlaneGeometry, greenMaterial5);
 
 
 
@@ -167,16 +110,16 @@ greenPlane1.rotation.set(THREE.MathUtils.degToRad(0), 0, 0); // Cambia los ángu
 greenPlane2.position.set(0, -10, 10); // Ajusta la posición para que sea paralelo al primer plano
 greenPlane2.rotation.set(THREE.MathUtils.degToRad(180), 0, 0); // Iguala la rotación al primer plano
 
-greenPlane3.position.set(-25, -10, 20); 
-greenPlane3.rotation.set(THREE.MathUtils.degToRad(0), 90, 0); 
+//greenPlane3.position.set(-25, -10, 20); 
+//greenPlane3.rotation.set(THREE.MathUtils.degToRad(0), 90, 0); 
 
-greenPlane4.position.set(25, -10, 20); 
-greenPlane4.rotation.set(THREE.MathUtils.degToRad(0), -90, 0); 
+//greenPlane4.position.set(25, -10, 20); 
+//greenPlane4.rotation.set(THREE.MathUtils.degToRad(0), -90, 0); 
 
-greenPlane5.position.set(0, -10, 50); 
-greenPlane5.rotation.set(THREE.MathUtils.degToRad(0), 0, 0); 
+//greenPlane5.position.set(0, -10, 50); 
+//greenPlane5.rotation.set(THREE.MathUtils.degToRad(0), 0, 0); 
 
-scene.add(greenPlane1, greenPlane2, greenPlane3, greenPlane4, greenPlane5); 
+scene.add(greenPlane1, greenPlane2); 
 
 
 
@@ -185,29 +128,7 @@ scene.add(greenPlane1, greenPlane2, greenPlane3, greenPlane4, greenPlane5);
 //FUNCIONES//
 function Colisiones(){
 	
-	if(cubeBB.intersectsBox(cube2BB)){
-		dl.intensity-=0.01;
-		sl.intensity-=0.01;
-		pl.intensity-=0.01;
-		
-	}
-	
-	if(cubeBB.intersectsBox(cube4BB)){
-		
-	
-		cube4.position.x=20;
-	}
-	
-		if(cubeBB.intersectsBox(cube3BB)){
-			vida-=20;
-			console.log("colision");
-			console.log("vida: " +vida);
-			move=false;
-			cube2.position.y = 100;
-			camera.position.z+=1;
-		}else{
-			move=true;
-		}
+
 }
 function animate() {
 	
@@ -217,34 +138,9 @@ function animate() {
 
 	//cube.rotation.x += 0.01;
 	//cube.rotation.y += 0.01;
-	cube.position.x = camera.position.x;
-	cube.position.z = camera.position.z - 2 ;
-	cube.position.y = -1 ;
 
-	cubeBB.copy(cube.geometry.boundingBox).applyMatrix4(cube.matrixWorld);
-	Colisiones();
+
 	
-
-if(cone.position.y > 1)
-{
-
-	cone.position.x -= 0.01;
-}
-
-if(cone.position.x < -1){
-
-cone.position.y -= 0.01;
-
-if(cone.position.y < -1){
-var entrar = true;
-}
-}
-
-if(cone.position.y< -1 && cone.position.x < 1){
-cone.position.x += 0.01;
-	
-}
-
 
 	renderer.render( scene, camera );
 	
@@ -265,20 +161,20 @@ const hiddenText = document.getElementById("hiddenText");
 
 
 if(tecla=='p'||tecla=='P'){
-	alert("POSICION:" +camera.position.x + "," + camera.position.z);
+	alert("POSICION:" +cube.position.x + "," + cube.position.z);
 	
 }
 if(tecla=='a'||tecla=='A'){
-	camera.position.x--;
+	cube.position.x--;
 }
 if(tecla=='d'||tecla=='D'){
-	camera.position.x++;
+	cube.position.x++;
 }
 if(tecla=='w'||tecla=='W'){
-	camera.position.z -=0.1;
+	cube.position.z--;
 }
 if(tecla=='s'||tecla=='S'){
-	camera.position.z +=0.1;
+	cube.position.z ++;
 }
 if(tecla=='c'||tecla=='C' ){
 	if(al.intensity<1)
@@ -300,26 +196,267 @@ if(tecla=='v'||tecla=='V'){
 });
 
 function Modelos3D(){
+//CASA//    											LUEGO HAGO LA FUNCION PARA MANDARLE SOLO LOS VALORES
+const granja = new OBJLoader(manager);
+var mtlGarnja = new MTLLoader(manager);
+// Molino
+  const Molino = new OBJLoader(manager);
+  var mtlMolino = new MTLLoader(manager);
+//Suelo
+const Suelo = new OBJLoader(manager);
+var mtlSuelo = new MTLLoader(manager);
+//Cultivo1
+const cultivo1 = new OBJLoader(manager);
+var mtlcultivo1 = new MTLLoader(manager);
+//cultivo2
+const cultivo2 = new OBJLoader(manager);
+var mtlcultivo2 = new MTLLoader(manager);
+const cultivo22 = new OBJLoader(manager);
+var mtlcultivo22 = new MTLLoader(manager);
+//montañas
+const mount = new OBJLoader(manager);
+var mtlmount = new MTLLoader(manager);
+const mount2 = new OBJLoader(manager);
+var mtlmount2 = new MTLLoader(manager);
+//Tractores
+const trac1 = new OBJLoader(manager);
+var mtltrac1 = new MTLLoader(manager);
+const trac2 = new OBJLoader(manager);
+var mtltrac2 = new MTLLoader(manager);
 
-  const flower1 = new OBJLoader(manager);
-  var mtlflower1 = new MTLLoader(manager);
 
- 
-
-mtlflower1.load('models/flower1.mtl',function (materials){
+mtltrac1.load('models/Tractor/Tractor.mtl',function (materials){
 
 	materials.preload();
 
-	flower1.setMaterials(materials);
+	trac1.setMaterials(materials);
 
-	flower1.load('models/flower1.obj',
+	trac1.load('models/Tractor/Tractor.obj',
 
 		function ( object ) {
 
-      object.scale.copy( new THREE.Vector3(1,1,1));
-	  object.position.x =0.0;
-	  object.position.y =0.0;
-	  object.position.z =0.0;
+      object.scale.copy( new THREE.Vector3(0.2,0.2,0.2));
+	  object.position.x =2;
+	  object.position.y =-60;
+	  object.position.z =30;
+	  object.rotation.y =98.8;
+	 
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+
+mtltrac2.load('models/Tractor/Tractor2.mtl',function (materials){
+
+	materials.preload();
+
+	trac2.setMaterials(materials);
+
+	trac2.load('models/Tractor/Tractor2.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(0.2,0.2,0.2));
+	  object.position.x =2;
+	  object.position.y =-60;
+	  object.position.z =15;
+	  object.rotation.y =98.8;
+	 
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+
+mtlcultivo2.load('models/Cultivo2/cultivo2.mtl',function (materials){
+
+	materials.preload();
+
+	cultivo2.setMaterials(materials);
+
+	cultivo2.load('models/Cultivo2/cultivo2.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(0.2,0.2,0.2));
+	  object.position.x =2;
+	  object.position.y =-60;
+	  object.position.z =40;
+	  object.rotation.y =99;
+	 
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+
+mtlmount2.load('models/Mount/mount.mtl',function (materials){
+
+	materials.preload();
+
+	mount2.setMaterials(materials);
+
+	mount2.load('models/Mount/mount.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(5,5,5));
+	  object.position.x =64;
+	  object.position.y =-60;
+	  object.position.z =-35;
+	  object.rotation.y =116.33;
+	 
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+
+mtlmount.load('models/Mount/mount.mtl',function (materials){
+
+	materials.preload();
+
+	mount.setMaterials(materials);
+
+	mount.load('models/Mount/mount.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(5,5,5));
+	  object.position.x =-69;
+	  object.position.y =-60;
+	  object.position.z =-48;
+	  object.rotation.y =116.33;
+	 
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+
+mtlcultivo2.load('models/Cultivo2/cultivo2.mtl',function (materials){
+
+	materials.preload();
+
+	cultivo2.setMaterials(materials);
+
+	cultivo2.load('models/Cultivo2/cultivo2.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(0.2,0.2,0.2));
+	  object.position.x =-12;
+	  object.position.y =-60;
+	  object.position.z =30;
+	  object.rotation.y =116.33;
+	 
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+ 
+mtlcultivo1.load('models/Cultivo1/cultivo1.mtl',function (materials){
+
+	materials.preload();
+
+	cultivo1.setMaterials(materials);
+
+	cultivo1.load('models/Cultivo1/cultivo1.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(0.3,0.3,0.3));
+	  object.position.x =15;
+	  object.position.y =-60;
+	  object.position.z =15;
+	  object.rotation.y =116.1;
+	 
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+
+mtlSuelo.load('models/suelo/suelo.mtl',function (materials){
+
+	materials.preload();
+
+	Suelo.setMaterials(materials);
+
+	Suelo.load('models/suelo/suelo.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(5,5,5));
+	  object.position.x =0;
+	  object.position.y =-66.5;
+	  object.position.z =0;
+	
+	
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+  
+mtlGarnja.load('models/granja/Farm.mtl',function (materials){
+
+	materials.preload();
+
+	granja.setMaterials(materials);
+
+	granja.load('models/granja/Farm.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(0.2,0.2,0.2));
+	  object.position.x =15;
+	  object.position.y =-60;
+	  object.position.z =35;
+	  object.rotation.y =116.1;
+	
+
+			scene.add( object );
+
+		});
+
+	console.log(materials);
+});
+ 
+
+mtlMolino.load('models/Molino/Molino.mtl',function (materials){
+
+	materials.preload();
+
+	Molino.setMaterials(materials);
+
+	Molino.load('models/Molino/Molino.obj',
+
+		function ( object ) {
+
+      object.scale.copy( new THREE.Vector3(0.2,0.2,0.2));
+	  object.position.x =-20;
+	  object.position.y =-60;
+	  object.position.z =10;
+	  object.rotation.y =116.1;
 	
 
 			scene.add( object );
@@ -405,62 +542,3 @@ alert("Conectado");
 })
 
 }
-
-
-/*
-	
-cone.rotation.x += 0.01;
-cone.rotation.y += 0.0;
-//REBOTE
-if(bandera == true){
-	
-if(cylinder.position.y<5){
-
-	cylinder.position.y += 0.2;
-}
-else{
-	bandera = false;
-}
-}
-else{
-
-	if(cylinder.position.y >0){
-
-		cylinder.position.y -=0.5;
-	}else{
-		bandera = true;
-		
-	}
-
-}
-
-if(cylinder.position.x < 6 && regreso == false){
-	cylinder.position.x += 0.01;
-	if(cylinder.position.x > 6){
-		regreso = true;
-	}
-}
-if(regreso==true){
-	cylinder.position.x -= 0.01;
-	if(cylinder.position.x < 2){
-	regreso=false;
-	}
-}
-
-
-//RECORRIDO
-if(esfera.position.x < -3){
-esfera.position.x +=0.01;
-pl.position.x +=0.01;
-}
-if(esfera.position.x > -3){
-
-	esfera.position.x = -5;
-	pl.position.x =-5;
-}
-=
-
-//ROTACION
-if(cone.position.x == 1){
-	cone.position.y += 0.01;
-}*/
