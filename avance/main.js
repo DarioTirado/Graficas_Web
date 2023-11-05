129adb899380129b2a3c9866b2f02011051d5d97
Roztank <rayfilemon@gmail.com>
2023-11-04 22:53:00 -0600

    Colisiones y cubos
    -Agregue cubos invisibles a los modelos de los tractores y cercas
    -Agregue colisiones entre tractores, falta mejorarla
    -Agregue colision con las cercas

--- avance/main.js
+++ avance/main.js
@@ -49,7 +49,77 @@ const cube = new THREE.Mesh( geometry, material );
 cube.position.set(0, -49, 50); 
 group.add( cube );
 
-//LUCES//
+var tractor_posicion = new THREE.Vector3(-11, -60, 35);
+var tractor_obj = new THREE.Object3D();
+
+//Cubos de colision
+
+const geometry_cube_tractor = new THREE.BoxGeometry( 2.3, 2.3, 2.3 );
+const material_cube_tractor = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const cube_tractor = new THREE.Mesh( geometry_cube_tractor, material_cube_tractor );
+cube_tractor.position.set(tractor_posicion.x, tractor_posicion.y, tractor_posicion.z);
+var cube_tractor_col = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+cube_tractor_col.setFromObject(cube_tractor);
+cube_tractor.visible = false;
+group.add( cube_tractor );
+
+var tractor2_posicion = new THREE.Vector3(9, -60, 35);
+var tractor2_obj = new THREE.Object3D();
+const geometry_cube_tractor2 = new THREE.BoxGeometry( 2.3, 2.3, 2.3 );
+const material_cube_tractor2 = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const cube_tractor2 = new THREE.Mesh( geometry_cube_tractor2, material_cube_tractor2 );
+cube_tractor2.position.set(tractor2_posicion.x, tractor2_posicion.y, tractor2_posicion.z);
+var cube_tractor2_col = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+cube_tractor2_col.setFromObject(cube_tractor2);
+cube_tractor2.visible = false;
+group.add( cube_tractor2 );
+
+var cerca_izquierda_posicion = new THREE.Vector3(-18, -60, 27);
+var cerca_izquierda_obj = new THREE.Object3D();
+const geometry_cube_cerca_izquierda = new THREE.BoxGeometry( 1, 2, 26 );
+const material_cube_cerca_izquierda = new THREE.MeshPhongMaterial( { color: 0x00ff10 } );
+const cube_cerca_izquierda = new THREE.Mesh( geometry_cube_cerca_izquierda, material_cube_cerca_izquierda );
+cube_cerca_izquierda.position.set(cerca_izquierda_posicion.x, cerca_izquierda_posicion.y, cerca_izquierda_posicion.z);
+var cerca_izquierda_col = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+cerca_izquierda_col.setFromObject(cube_cerca_izquierda);
+cube_cerca_izquierda.visible = false;
+group.add( cube_cerca_izquierda );
+
+var cerca_derecha_posicion = new THREE.Vector3(15, -60, 27);
+var cerca_derecha_obj = new THREE.Object3D();
+const geometry_cube_cerca_derecha = new THREE.BoxGeometry( 1, 2, 26 );
+const material_cube_cerca_derecha = new THREE.MeshPhongMaterial( { color: 0x00ff10 } );
+const cube_cerca_derecha = new THREE.Mesh( geometry_cube_cerca_derecha, material_cube_cerca_derecha );
+cube_cerca_derecha.position.set(cerca_derecha_posicion.x, cerca_derecha_posicion.y, cerca_derecha_posicion.z);
+var cerca_derecha_col = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+cerca_derecha_col.setFromObject(cube_cerca_derecha);
+cube_cerca_derecha.visible = false;
+group.add( cube_cerca_derecha );
+
+var cerca_arriba_posicion = new THREE.Vector3(-3, -60, 12);
+var cerca_arriba_obj = new THREE.Object3D();
+const geometry_cube_cerca_arriba = new THREE.BoxGeometry( 32, 2, 1 );
+const material_cube_cerca_arriba = new THREE.MeshPhongMaterial( { color: 0x00ff10 } );
+const cube_cerca_arriba = new THREE.Mesh( geometry_cube_cerca_arriba, material_cube_cerca_arriba );
+cube_cerca_arriba.position.set(cerca_arriba_posicion.x, cerca_arriba_posicion.y, cerca_arriba_posicion.z);
+var cerca_arriba_col = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+cerca_arriba_col.setFromObject(cube_cerca_arriba);
+cube_cerca_arriba.visible = false;
+group.add( cube_cerca_arriba );
+
+var cerca_abajo_posicion = new THREE.Vector3(-3, -60, 41);
+var cerca_abajo_obj = new THREE.Object3D();
+const geometry_cube_cerca_abajo = new THREE.BoxGeometry( 32, 2, 1 );
+const material_cube_cerca_abajo = new THREE.MeshPhongMaterial( { color: 0x00ff10 } );
+const cube_cerca_abajo = new THREE.Mesh( geometry_cube_cerca_abajo, material_cube_cerca_abajo );
+cube_cerca_abajo.position.set(cerca_abajo_posicion.x, cerca_abajo_posicion.y, cerca_abajo_posicion.z);
+var cerca_abajo_col = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+cerca_abajo_col.setFromObject(cube_cerca_abajo);
+cube_cerca_abajo.visible = false;
+group.add( cube_cerca_abajo );
+
+
+//////////////////LUCES/////////////////////77
 const al = new THREE.AmbientLight(0xffffff, 0.5);
 group.add(al);
 
@@ -151,10 +221,17 @@ var mtlcerca_adl = new MTLLoader(manager);
 //Tractores
 const trac1 = new OBJLoader(manager);
 var mtltrac1 = new MTLLoader(manager);
-let object1;
 const trac2 = new OBJLoader(manager);
 var mtltrac2 = new MTLLoader(manager);
-let object2;
+//Colisiones_variables
+var move_tractor_izquierda = true;
+var move_tractor_derecha = true;
+var move_tractor_arriba = true;
+var move_tractor_abajo = true;
+var move_tractor2_izquierda = true;
+var move_tractor2_derecha = true;
+var move_tractor2_arriba = true;
+var move_tractor2_abajo = true;
 
 mtltrac1.load('models/Tractor/Tractor.mtl',function (materials){
 
@@ -165,11 +242,10 @@ mtltrac1.load('models/Tractor/Tractor.mtl',function (materials){
 	trac1.load('models/Tractor/Tractor.obj',
 
 		function ( object ) {
-			object1=object;
-			object1.scale.copy(new THREE.Vector3(0.12, 0.12, 0.12));
-			object1.position.set(-11, -60, 35);
-			object1.rotation.y = 98.8;
-			scene.add(object1);
+			object.scale.copy(new THREE.Vector3(0.12, 0.12, 0.12));
+			object.rotation.y = 98.8;
+			scene.add(object);
+			tractor_obj = object;
 		});
 
 	console.log(materials);
@@ -184,11 +260,10 @@ mtltrac2.load('models/Tractor/Tractor2.mtl',function (materials){
 	trac2.load('models/Tractor/Tractor2.obj',
 
 		function ( object ) {
-			object2=object;
-      object2.scale.copy( new THREE.Vector3(0.12, 0.12, 0.12));
-	  object2.position.set(9, -60, 35);
-	  object2.rotation.y =98.8;
-	   scene.add( object2 );
+      object.scale.copy( new THREE.Vector3(0.12, 0.12, 0.12));
+	  object.rotation.y =98.8;
+	   scene.add( object );
+	   tractor2_obj = object;
 
 });
 
@@ -212,7 +287,7 @@ mtlcultivo2.load('models/Cultivo2/cultivo2.mtl',function (materials){
 	  object.rotation.y =110;
 	 
 
-			scene.add( object );
+			//scene.add( object );
 
 		});
 
@@ -236,7 +311,7 @@ mtlmount2.load('models/Mount/mount.mtl',function (materials){
 	  object.rotation.y =116.33;
 	 
 
-			scene.add( object );
+			//scene.add( object );
 
 		});
 
@@ -260,7 +335,7 @@ mtlmount.load('models/Mount/mount.mtl',function (materials){
 	  object.rotation.y =116.33;
 	 
 
-			scene.add( object );
+			//scene.add( object );
 
 		});
 
@@ -284,7 +359,7 @@ mtlcultivo2.load('models/Cultivo2/cultivo2.mtl',function (materials){
 	  object.rotation.y =116.33;
 	 
 
-			scene.add( object );
+			//scene.add( object );
 
 		});
 
@@ -332,7 +407,7 @@ mtlGarnja.load('models/granja/Farm.mtl',function (materials){
 	  object.rotation.y =116.1;
 	
 
-			scene.add( object );
+			//scene.add( object );
 
 		});
 
@@ -357,7 +432,7 @@ mtlMolino.load('models/Molino/Molino.mtl',function (materials){
 	  object.rotation.y =116.1;
 	
 
-			scene.add( object );
+			//scene.add( object );
 
 		});
 
@@ -418,7 +493,7 @@ mtlplanta.load('models/planta.mtl',function (materials){
 	object.position.z =0.0;
   
 
-		  scene.add( object );
+		  //scene.add( object );
 
 	  });
 
@@ -437,14 +512,11 @@ mtlcerca_izq.load('models/Cerca/Cerca_1.mtl',function (materials){
 	  function ( object ) {
 
 	object.scale.copy( new THREE.Vector3(0.4,0.33,0.4));
-	 object.position.x =-18;
-	  object.position.y =-60;
-	  object.position.z =27;
 	  object.rotation.y =99;
   
 
 		  scene.add( object );
-
+		  cerca_izquierda_obj = object;
 	  });
 
   console.log(materials);
@@ -462,14 +534,11 @@ mtlcerca_der.load('models/Cerca/Cerca_1.mtl',function (materials){
 		function ( object ) {
   
 	  object.scale.copy( new THREE.Vector3(0.4,0.33,0.4));
-	   object.position.x =15;
-		object.position.y =-60;
-		object.position.z =27;
 		object.rotation.y =99;
 	
   
 			scene.add( object );
-  
+			cerca_derecha_obj = object;
 		});
   
 	console.log(materials);
@@ -487,13 +556,11 @@ mtlcerca_der.load('models/Cerca/Cerca_1.mtl',function (materials){
 		function ( object ) {
   
 	  object.scale.copy( new THREE.Vector3(0.42,0.33,0.4));
-	   object.position.x =-3;
-		object.position.y =-60;
-		object.position.z =12;
 		object.rotation.y =110;
 	
   
 			scene.add( object );
+			cerca_arriba_obj= object;
   
 		});
   
@@ -512,13 +579,11 @@ mtlcerca_der.load('models/Cerca/Cerca_1.mtl',function (materials){
 		function ( object ) {
   
 	  object.scale.copy( new THREE.Vector3(0.43,0.33,0.4));
-	   object.position.x =-1;
-		object.position.y =-60;
-		object.position.z =41;
 		object.rotation.y =110;
 	
   
 			scene.add( object );
+			cerca_abajo_obj=object;
   
 		});
   
@@ -528,17 +593,71 @@ mtlcerca_der.load('models/Cerca/Cerca_1.mtl',function (materials){
 
 
 //-----------------------------------------FUNCIONES--------------------------------------------------------
-function Colisiones(){
-	
 
-}
 function animate() {
 	
+	//Jugador 1
+	cube_tractor.position.x = tractor_posicion.x;
+	cube_tractor.position.y = tractor_posicion.y;
+	cube_tractor.position.z = tractor_posicion.z;
+
+	tractor_obj.position.x = tractor_posicion.x;
+	tractor_obj.position.y = tractor_posicion.y;
+	tractor_obj.position.z = tractor_posicion.z;
+
+	//Jugador 2
+	cube_tractor2.position.x = tractor2_posicion.x;
+	cube_tractor2.position.y = tractor2_posicion.y;
+	cube_tractor2.position.z = tractor2_posicion.z;
+
+	tractor2_obj.position.x = tractor2_posicion.x;
+	tractor2_obj.position.y = tractor2_posicion.y;
+	tractor2_obj.position.z = tractor2_posicion.z;
+
+	cube_tractor_col.setFromObject(cube_tractor);
+	cube_tractor_col.copy(cube_tractor.geometry.boundingBox).applyMatrix4(cube_tractor.matrixWorld);
+	cube_tractor2_col.setFromObject(cube_tractor2);
+	cube_tractor2_col.copy(cube_tractor2.geometry.boundingBox).applyMatrix4(cube_tractor2.matrixWorld);
+
+	//Ubicamos cercas con sus cubos
+	cube_cerca_izquierda.position.x = cerca_izquierda_posicion.x;
+	cube_cerca_izquierda.position.y = cerca_izquierda_posicion.y;
+	cube_cerca_izquierda.position.z = cerca_izquierda_posicion.z;
+  
+	cerca_izquierda_obj.position.x = cerca_izquierda_posicion.x;
+	cerca_izquierda_obj.position.y = cerca_izquierda_posicion.y;
+	cerca_izquierda_obj.position.z = cerca_izquierda_posicion.z;
+  
+	cube_cerca_derecha.position.x = cerca_derecha_posicion.x;
+	cube_cerca_derecha.position.y = cerca_derecha_posicion.y;
+	cube_cerca_derecha.position.z = cerca_derecha_posicion.z;
+  
+	cerca_derecha_obj.position.x = cerca_derecha_posicion.x;
+	cerca_derecha_obj.position.y = cerca_derecha_posicion.y;
+	cerca_derecha_obj.position.z = cerca_derecha_posicion.z;
+  
+	cube_cerca_arriba.position.x = cerca_arriba_posicion.x + 1;
+	cube_cerca_arriba.position.y = cerca_arriba_posicion.y;
+	cube_cerca_arriba.position.z = cerca_arriba_posicion.z;
+  
+	cerca_arriba_obj.position.x = cerca_arriba_posicion.x + 1;
+	cerca_arriba_obj.position.y = cerca_arriba_posicion.y;
+	cerca_arriba_obj.position.z = cerca_arriba_posicion.z;
+
+	cube_cerca_abajo.position.x = cerca_abajo_posicion.x + 1;
+	cube_cerca_abajo.position.y = cerca_abajo_posicion.y;
+	cube_cerca_abajo.position.z = cerca_abajo_posicion.z;
+  
+	cerca_abajo_obj.position.x = cerca_abajo_posicion.x + 1;
+	cerca_abajo_obj.position.y = cerca_abajo_posicion.y;
+	cerca_abajo_obj.position.z = cerca_abajo_posicion.z;
+
+	Colisiones();
+
 	requestAnimationFrame( animate );
 
 	//cube.rotation.x += 0.01;
 	//cube.rotation.y += 0.01;
-
 
 	
 
@@ -564,63 +683,187 @@ if(tecla=='p'||tecla=='P'){
 }
 
 //Jugador 1
+
+if(tecla=='e'||tecla=='E'){
+	tractor_posicion.x=-11;
+	tractor_posicion.z=35;
+	tractor_obj.rotation.y=98.8;
+	//cube_tractor.rotation.y=270;
+	move_tractor_izquierda = true;
+	move_tractor_derecha = true;
+	move_tractor_arriba = true;
+	move_tractor_abajo = true;
+}
 if(tecla=='a'||tecla=='A'){
 	//cube.position.x--;
-	if (object1) {
-		object1.position.x-=0.7;
-		object1.rotation.y=270;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor_derecha)||(!move_tractor_arriba)||(!move_tractor_abajo)){
+			tractor_posicion.x-=1;
+		    tractor_obj.rotation.y=270;
+		    //cube_tractor.rotation.y=98.8;
+			move_tractor_derecha=true;
+			move_tractor_arriba=true;
+			move_tractor_abajo=true;
+		}else{
+			move_tractor_izquierda=false;
+		}
+	}else{
+		tractor_posicion.x-=0.7;
+		tractor_obj.rotation.y=270;
+		//cube_tractor.rotation.y=98.8;
+		move_tractor_izquierda=false;
+	}
 }
 if(tecla=='d'||tecla=='D'){
 	//cube.position.x++;
-	if (object1) {
-		object1.position.x+=0.7;
-		object1.rotation.y=110;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor_izquierda)||(!move_tractor_arriba)||(!move_tractor_abajo)){
+			tractor_posicion.x+=1;
+	        tractor_obj.rotation.y=110;
+	        //cube_tractor.rotation.y=20.41;
+			move_tractor_izquierda=true;
+			move_tractor_arriba=true;
+			move_tractor_abajo=true;
+		}else{
+			move_tractor_derecha=false;
+		}
+	}else{
+		tractor_posicion.x+=0.7;
+	    tractor_obj.rotation.y=110;
+	    //cube_tractor.rotation.y=20.41;
+		move_tractor_derecha=false;
+	}
 }
 if(tecla=='w'||tecla=='W'){
 	//cube.position.z--;
-	if (object1) {
-		object1.position.z-=0.7;
-		object1.rotation.y=98.8;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor_izquierda)||(!move_tractor_derecha)||(!move_tractor_abajo)){
+			tractor_posicion.z-=1;
+		    tractor_obj.rotation.y=98.8;
+		    //cube_tractor.rotation.y=270;
+			move_tractor_izquierda=true;
+			move_tractor_derecha=true;
+			move_tractor_abajo=true;
+		}else{
+			move_tractor_arriba=false;
+		}
+	}else{
+		tractor_posicion.z-=0.7;
+		tractor_obj.rotation.y=98.8;
+		//cube_tractor.rotation.y=270;
+		move_tractor_arriba=false;
+	}
 }
 if(tecla=='s'||tecla=='S'){
 	//cube.position.z ++;
-	if (object1) {
-		object1.position.z+=0.7;
-		object1.rotation.y=20.41;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor_izquierda)||(!move_tractor_derecha)||(!move_tractor_arriba)){
+			tractor_posicion.z+=1;
+	        tractor_obj.rotation.y=20.41;
+	        //cube_tractor.rotation.y=110;
+			move_tractor_izquierda=true;
+			move_tractor_derecha=true;
+			move_tractor_arriba=true;
+		}else{
+			move_tractor_abajo=false;
+		}
+	}else{
+	    tractor_posicion.z+=0.7;
+	    tractor_obj.rotation.y=20.41;
+	    //cube_tractor.rotation.y=110;
+		move_tractor_abajo=false;
+	}
 }
 
 //Jugador 2
+if(tecla=='o'||tecla=='O'){
+	tractor2_posicion.x=9;
+	tractor2_posicion.z=35;
+	tractor2_obj.rotation.y=98.8;
+	move_tractor2_izquierda = true;
+	move_tractor2_derecha = true;
+	move_tractor2_arriba = true;
+	move_tractor2_abajo = true;
+}
 if(tecla=='j'||tecla=='J'){
 	//cube.position.x--;
-	if (object2) {
-		object2.position.x-=0.7;
-		object2.rotation.y=270;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor2_derecha)||(!move_tractor2_arriba)||(!move_tractor2_abajo)){
+			tractor2_posicion.x-=1;
+		    tractor2_obj.rotation.y=270;
+			move_tractor2_derecha=true;
+			move_tractor2_arriba=true;
+			move_tractor2_abajo=true;
+		}else{
+			move_tractor2_izquierda=false;
+		}
+	}else{
+		tractor2_posicion.x-=0.7;
+		tractor2_obj.rotation.y=270;
+		move_tractor2_izquierda=false;
+	}
 }
 if(tecla=='l'||tecla=='L'){
 	//cube.position.x++;
-	if (object2) {
-		object2.position.x+=0.7;
-		object2.rotation.y=110;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor2_izquierda)||(!move_tractor2_arriba)||(!move_tractor2_abajo)){
+			tractor2_posicion.x+=1;
+	        tractor2_obj.rotation.y=110;
+			move_tractor2_izquierda=true;
+			move_tractor2_arriba=true;
+			move_tractor2_abajo=true;
+		}else{
+			move_tractor2_derecha=false;
+		}
+	}else{
+		tractor2_posicion.x+=0.7;
+	    tractor2_obj.rotation.y=110;
+		move_tractor2_derecha=false;
+	}
 }
 if(tecla=='i'||tecla=='I'){
 	//cube.position.z--;
-	if (object2) {
-		object2.position.z-=0.7;
-		object2.rotation.y=98.8;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor2_izquierda)||(!move_tractor2_derecha)||(!move_tractor2_abajo)){
+			tractor2_posicion.z-=1;
+		    tractor2_obj.rotation.y=98.8;
+			move_tractor2_izquierda=true;
+			move_tractor2_derecha=true;
+			move_tractor2_abajo=true;
+		}else{
+			move_tractor2_arriba=false;
+		}
+	}else{
+		tractor2_posicion.z-=0.7;
+		tractor2_obj.rotation.y=98.8;
+		move_tractor2_arriba=false;
+	}
 }
 if(tecla=='k'||tecla=='K'){
 	//cube.position.z ++;
-	if (object2) {
-		object2.position.z+=0.7;
-		object2.rotation.y=20.41;
-	} 
+	if(cube_tractor_col.intersectsBox(cube_tractor2_col))
+	{
+		if((!move_tractor2_izquierda)||(!move_tractor2_derecha)||(!move_tractor2_arriba)){
+			tractor2_posicion.z+=1;
+	        tractor2_obj.rotation.y=20.41;
+			move_tractor2_izquierda=true;
+			move_tractor2_derecha=true;
+			move_tractor2_arriba=true;
+		}else{
+			move_tractor2_abajo=false;
+		}
+	}else{
+	    tractor2_posicion.z+=0.7;
+	    tractor2_obj.rotation.y=20.41;
+		move_tractor2_abajo=false;
+	}
 }
 
 if(tecla=='c'||tecla=='C' ){
@@ -641,6 +884,45 @@ if(tecla=='v'||tecla=='V'){
 
 
 });
+
+function Colisiones(){
+	
+	if(cube_tractor_col.intersectsBox(cerca_izquierda_col))
+	{
+		tractor_posicion.x+=1;
+	}
+	if(cube_tractor2_col.intersectsBox(cerca_izquierda_col))
+	{
+		tractor2_posicion.x+=1;
+	}
+
+	if(cube_tractor_col.intersectsBox(cerca_derecha_col))
+	{
+		tractor_posicion.x-=1;
+	}
+	if(cube_tractor2_col.intersectsBox(cerca_derecha_col))
+	{
+		tractor2_posicion.x-=1;
+	}
+
+	if(cube_tractor_col.intersectsBox(cerca_arriba_col))
+	{
+		tractor_posicion.z+=1;
+	}
+	if(cube_tractor2_col.intersectsBox(cerca_arriba_col))
+	{
+		tractor2_posicion.z+=1;
+	}
+
+	if(cube_tractor_col.intersectsBox(cerca_abajo_col))
+	{
+		tractor_posicion.z-=1;
+	}
+	if(cube_tractor2_col.intersectsBox(cerca_abajo_col))
+	{
+		tractor2_posicion.z-=1;
+	}
+}
 
 function iniciarSesion(nombre, contra){
 alert(nombre, contra);
